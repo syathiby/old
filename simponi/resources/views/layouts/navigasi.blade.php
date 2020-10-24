e3b064583f98bcc486dcb50d3cdb76cc211f8b6e
Alendia Setiawan <alendia.setiawan@gmail.com>
2020-10-24 09:02:10 +0700

    berhasil

--- simponi/resources/views/layouts/navigasi.blade.php
+++ simponi/resources/views/layouts/navigasi.blade.php
@@ -1,7 +1,7 @@
 <div>
     <a href="/">Home</a>
 
-    <a href="administrasi">Administrasi</a>
+    <a href="administrasi">Data Admin</a>
 
     <a href="informasi-akademik">Informasi Akademik</a>
 
